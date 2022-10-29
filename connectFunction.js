355c8d88045f39055b60bc7e145c86f12359745b
Jplp1919 <88355957+Jplp1919@users.noreply.github.com>
2022-10-29 14:38:26 -0300

    Add files via upload

--- connectFunction.js
+++ connectFunction.js
@@ -60,3 +60,52 @@
       console.error(result);
     });
   }
+
+
+  function updateValorProduto(idproduto, valor){
+    var produto ={
+      idproduto: idproduto
+    }
+    var valor ={
+      valor: valor
+    }
+    var query = connection.query('UPDATE PRODUTO SET valor = ? WHERE id = ?', valor, idproduto, function (err, result){
+      if (err) {
+    console.error(err);
+    return;
+  }
+  console.error(result);
+});
+  }
+
+  function updateEstoqueProduto(idproduto, quantidade){
+    var produto ={
+      idproduto: idproduto
+    }
+    var quantidade={
+      quantidade: quantidade
+    }
+    var query = connection.query('UPDATE PRODUTO SET estoque = ? WHERE id = ?', quantidade, idproduto, function (err, result){
+      if (err) {
+    console.error(err);
+    return;
+  }
+  console.error(result);
+});
+  }
+
+  function updateNomeProduto(idproduto, nome){
+    var produto ={
+      idproduto: idproduto
+    }
+    var nome={
+      nome: nome
+    }
+    var query = connection.query('UPDATE PRODUTO SET nome = ? WHERE id = ?', nome, idproduto, function (err, result){
+      if (err) {
+    console.error(err);
+    return;
+  }
+  console.error(result);
+});
+  }
